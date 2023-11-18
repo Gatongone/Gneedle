23a614e2775e5d239bdd434b29785e10933be8b5
Gatongone <gatongone@gmail.com>
2023-11-18 23:30:00 +0800

    sparkles: add assembly visible information to test project.

---
+++ Gneedle.Inject/Property/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿/*
+ * Copyright ©2023 Gatongone
+ * Author: Gatongone
+ * Email: gatongone@gmail.com
+ * Created On: 2023/11/18-13:16:57
+ * Github: https://github.com/Gatongone
+ */
+
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Gneedle.Test")]
