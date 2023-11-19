2437a54677401e7ba165581dbcb0b5a582db4e22
Gatongone <gatongone@gmail.com>
2023-11-19 23:46:48 +0800

    feature: type and typename.

---
+++ Gneedle.Inject/Needle/Type.cs
@@ -0,0 +1,178 @@
+﻿/*
+ * Copyright ©2023 Gatongone
+ * Author: Gatongone
+ * Email: gatongone@gmail.com
+ * Created On: 2023/11/18-11:47:36
+ * Github: https://github.com/Gatongone
+ */
+
+namespace Gneedle.Inject;
+
+/// <summary>
+/// Type used for describing class generic argument, method generic argument and method parameter type information.
+/// </summary>
+public interface IType;
+
+/// <summary>
+/// Generic parameter type.
+/// </summary>
+/// <param name="name">The generic parameter type name.</param>
+/// <param name="constraints">Generic parameter type </param>
+public sealed class GenericParameterType(string name, params Constraint[] constraints) : IType
+{
+    /// <summary>
+    /// The generic parameter type name.
+    /// </summary>
+    public readonly string TypeName = name;
+
+    /// <summary>
+    /// The generic parameter constraints.
+    /// </summary>
+    public readonly Constraint[] Constraints = constraints;
+
+    /// <summary>
+    /// Create a generic parameter type from name.
+    /// </summary>
+    /// <param name="name">The generic parameter type name.</param>
+    public GenericParameterType(string name) : this(name, Array.Empty<Constraint>()) { }
+    
+    /// <summary>
+    /// Get type name.
+    /// </summary>
+    /// <returns>Type name.</returns>
+    public override string ToString() => TypeName;
+}
+
+/// <summary>
+/// Type without any generic arguments.
+/// </summary>
+public sealed class NongenericType : IType
+{
+    /// <summary>
+    /// Type without any generic arguments.
+    /// </summary>
+    public readonly Type Type;
+
+    /// <summary>
+    /// Create a non generic type from system type.
+    /// </summary>
+    /// <param name="type">System type with non generic.</param>
+    /// <exception cref="ArgumentException">Throw when the <c>type</c> is type.</exception>
+    public NongenericType(Type type)
+    {
+        if (type.IsGenericType)
+            throw new ArgumentException(string.Format(ErrorResources.IS_NOT_NON_GENERIC_PARAMETER_TYPE, type.FullName));
+        Type = type;
+    }
+
+    public static implicit operator NongenericType(Type type) => new(type);
+
+    /// <summary>
+    /// Get type name.
+    /// </summary>
+    /// <returns>Type name.</returns>
+    public override string ToString() => Type.FullName!;
+}
+
+/// <summary>
+/// Type with generic arguments. Samples:
+/// <list>
+/// <item>√ MyClass&lt;T1, T2&gt;</item>
+/// <item>√ MyClass&lt;int, string&gt;</item>
+/// <item>√ MyClass&lt;T, string&gt;</item>
+/// <item>× MyClass</item>
+/// </list>
+/// </summary>
+public sealed class GenericType : IType
+{
+    /// <summary>
+    /// Generic type definition.
+    /// </summary>
+    public readonly Type Type;
+
+    /// <summary>
+    /// The arguments for the generic type.
+    /// </summary>
+    public readonly IType[] GenericArguments;
+
+    /// <summary>
+    /// Create generic type with generic parameter type arguments.
+    /// <example>
+    /// If you ganna make <c>MyClass&lt;T1, T2&gt;</c>, you can use:
+    /// <code>new Generic(typeof(MyClass&lt;,&gt;), "T1", "T2");</code>
+    /// </example>
+    /// </summary>
+    /// <param name="type">Generic type definition.</param>
+    /// <param name="argumentNames">Generic parameter type arguments</param>
+    public GenericType(Type type, params string[] argumentNames) : this(type, argumentNames.Select(str => new GenericParameterType(str)).ToArray()) { }
+
+    /// <summary>
+    /// Create generic type with any generic arguments.
+    /// <example>
+    /// If you ganna make <c>MyClass&lt;int, string&gt;</c>, you can use:
+    /// <code>new Generic(typeof(MyClass&lt;,&gt;), typeof(int), typeof(string));</code>
+    /// </example>
+    /// </summary>
+    /// <param name="type">Generic type definition.</param>
+    /// <param name="argumentTypes">Generic argument Types.</param>
+    public GenericType(Type type, params Type[] argumentTypes) : this(type, argumentTypes.Select(p =>
+    {
+        if (!p.ContainsGenericParameters && p.GenericTypeArguments.Length == 0) return new NongenericType(p);
+        if (p.IsGenericParameter) return new GenericParameterType(p.Name);
+        return new GenericType(p, p.GetGenericArguments()) as IType;
+    }).ToArray()) { }
+
+    /// <summary>
+    /// Create generic type with any generic arguments.
+    /// <example>
+    /// If you ganna make <c>MyClass&lt;T, string&gt;</c>, you can use:
+    /// <code>new Generic(typeof(MyClass&lt;,&gt;), new GenericParameterType("T"), new NongenericType(typeof(string)));</code>
+    /// </example>
+    /// </summary>
+    /// <param name="type">Generic type definition.</param>
+    /// <param name="genericArguments">Generic arguments.</param>
+    /// <exception cref="ArgumentException">Throw when the <c>type</c> is not generic type.</exception>
+    public GenericType(Type type, params IType[] genericArguments)
+    {
+        if (!type.IsGenericType)
+            throw new ArgumentException(string.Format(ErrorResources.IS_NOT_PARAMETERIZED_GENERIC_TYPE, type.FullName));
+        Type = type.GetGenericTypeDefinition();
+        GenericArguments = genericArguments;
+    }
+
+    /// <summary>
+    /// Get type name.
+    /// </summary>
+    /// <returns>Type name.</returns>
+    public override string ToString() => this.GetTypeName().ToString();
+}
+
+/// <summary>
+/// Extension for <see cref="IType"/>.
+/// </summary>
+public static class TypeExtensions
+{
+    /// <summary>
+    /// Create <see cref="IType"/> from System.Type.
+    /// </summary>
+    /// <param name="type">The type where create from.</param>
+    /// <returns><see cref="IType"/></returns>
+    public static IType ToGneedleType(this Type type)
+    {
+        if (type.IsGenericParameter)
+            return new GenericParameterType(type.Name);
+        if (!type.IsGenericType)
+            return new NongenericType(type);
+        return new GenericType(type, type
+            .GetGenericArguments()
+            .Select(argument => argument.ToGneedleType())
+            .ToArray());
+    }
+
+    /// <summary>
+    /// Get type's name.
+    /// </summary>
+    /// <param name="type">The name owner.</param>
+    /// <returns>Type name.</returns>
+    public static TypeName GetTypeName(this IType type) => new(type);
+}
---
+++ Gneedle.Inject/Needle/TypeName.cs
@@ -0,0 +1,167 @@
+﻿/*
+ * Copyright ©2023 Gatongone
+ * Author: Gatongone
+ * Email: gatongone@gmail.com
+ * Created On: 2023/11/18-11:46:50
+ * Github: https://github.com/Gatongone
+ */
+
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Gneedle.Inject;
+
+/// <summary>
+/// Type name where read from <see cref="System.Type"/>, <see cref="Mono.Cecil.TypeReference"/> and <see cref="Gneedle.Inject.IType"/>.
+/// The type name could be used for getting TypeReference by <see cref="Mono.Cecil.ModuleDefinition.GetType(string)">Mono.Cecil.ModuleDefinition.GetType(string)</see>.
+/// or getting runtime type by <see cref="System.Type.GetType(string)">System.Type.GetType(string)</see><para/>
+/// 
+/// It will follow the following naming rules:
+/// <code>
+/// TypeName = Type.IsGenericType
+/// ? "{Namespace}.{NestedTypeName}+{TypeName}"
+/// : Type.ContainsGenericParameters &amp;&amp; !Type.ContainsGenericTypeArguments
+///     ? "{Namespace}.{NestedTypeName}+{TypeName}`{GenericArgumentsCount}"
+///     :  "{Namespace}.{NestedTypeName}+{TypeName}`{GenericArgumentsCount}[Argument1,Argument2...ArgumentN]"
+/// </code>
+/// <example>
+/// <list>
+/// <item><see cref="System.Collections.Generic.List{T}.Enumerator">List&lt;T&gt;.Enumerator</see> => System.Collections.Generic.List`1+Enumerator</item>
+/// <item><see cref="System.Collections.Generic.List{T}"/> => System.Collections.Generic.List`1</item>
+/// <item><see cref="System.Collections.Generic.List{T}">List&lt;int&gt;</see> => System.Collections.Generic.List`1[System.Int32]</item>
+/// <item><see cref="System.Collections.Generic.List{T}">List&lt;List&lt;T&gt;&gt;</see> => System.Collections.Generic.List`1[System.Collections.Generic.List`1[T]]</item>
+/// <item><see cref="System.Collections.Generic.Dictionary{TKey, TValue}"/> => System.Collections.Generic.Dictionary`2</item>
+/// <item><see cref="System.Collections.Generic.Dictionary{TKey, TValue}">Dictionary&lt;string, int&gt;</see> => System.Collections.Generic.Dictionary`2[System.String, System.Int32]</item>
+/// </list>
+/// </example>
+/// </summary>
+public readonly struct TypeName : IEquatable<TypeName>
+{
+    /// <summary>
+    /// Type name.
+    /// </summary>
+    public readonly string Name;
+
+    /// <summary>
+    /// Create type name from <see cref="System.Type"/>.
+    /// </summary>
+    /// <param name="type">The name owner.</param>
+    public TypeName(Type type)
+    {
+        if (type.IsGenericType)
+        {
+            Name = type.ContainsGenericParameters && type.GenericTypeArguments.Length == 0
+                ? type.FullName ?? type.ToString()
+                : Regex.Replace(type.ToString(), @", .*?]", "]");
+        }
+        else
+        {
+            Name = type.ToString();
+        }
+    }
+
+    /// <summary>
+    /// Create type name from <see cref="Mono.Cecil.TypeReference"/>.
+    /// </summary>
+    /// <param name="typeRef">The name owner.</param>
+    public TypeName(TypeReference typeRef)
+    {
+        if (typeRef is GenericInstanceType)
+        {
+            var stringBuilder = new StringBuilder();
+            var fullName = typeRef.FullName;
+            for (var index = 0; index < fullName.Length; index++)
+            {
+                var c = fullName[index];
+                switch (c)
+                {
+                    case '<':
+                        stringBuilder.Append("[");
+                        break;
+                    case ',':
+                        stringBuilder.Append(",");
+                        break;
+                    case '>':
+                        stringBuilder.Append("]");
+                        break;
+                    default:
+                        stringBuilder.Append(c);
+                        break;
+                }
+            }
+
+            Name = stringBuilder.ToString();
+        }
+        else
+            Name = typeRef.FullName;
+    }
+    
+    /// <inheritdoc cref="TypeName(IType,bool)"/>
+    public TypeName(IType type) : this(type, true) { }
+
+    /// <summary>
+    /// Create type name from <see cref="Gneedle.Inject.IType"/>.
+    /// </summary>
+    /// <exception cref="ArgumentException">The parameter type is not following type:
+    /// <list>
+    /// <item><see cref="GenericParameterType"/></item>
+    /// <item><see cref="NongenericType"/></item>
+    /// <item><see cref="GenericType"/></item>
+    /// </list>
+    /// </exception>
+    /// <param name="type">The name owner.</param>
+    /// <param name="isFirstIn">
+    /// Flag for making the generic type drops first argument which not contains any generic parameters.<para/>
+    /// Such like the type name of <c>MyClass&lt;T&gt;</c> should be <c>MyClass'1</c> but not <c>MyClass'1[T].</c><para/>
+    /// And also like the type name of <c>MyClass&lt;MyClass&lt;T&gt;&gt;</c> should be <c>MyClass'1[MyClass'1[T]]</c> but not <c>MyClass'1[MyClass'1]</c> or <c>MyClass'1</c><para/>
+    /// </param>
+    private TypeName(IType type, bool isFirstIn)
+    {
+        switch (type)
+        {
+            case GenericParameterType genericParameterType:
+                Name = genericParameterType.TypeName;
+                break;
+            case NongenericType nonGenericParameterType:
+                Name = nonGenericParameterType.Type.FullName ?? nonGenericParameterType.Type.Name;
+                break;
+            case GenericType parameterizedGenericType:
+            {
+                var arguments = parameterizedGenericType.GenericArguments;
+                Name = parameterizedGenericType.Type.FullName ?? parameterizedGenericType.Type.Name;
+                
+                // If first in, then check if contains any generic parameter.
+                if (isFirstIn && parameterizedGenericType.GenericArguments.All(arg => arg is GenericParameterType)) return;
+                
+                // Else keep append arguments info to string builder.
+                var stringBuilder = new StringBuilder();
+                stringBuilder.Append(Name);
+                stringBuilder.Append("[");
+                for (var i = 0; i < arguments.Length; i++)
+                {
+                    // Recursive create typename.
+                    stringBuilder.Append(new TypeName(arguments[i], false).ToString());
+                    if (i != arguments.Length - 1)
+                        stringBuilder.Append(",");
+                }
+                stringBuilder.Append("]");
+                Name = stringBuilder.ToString();
+
+                break;
+            }
+            default: throw new ArgumentException(string.Format(ErrorResources.INVALID_TYPE_NAME));
+        }
+    }
+
+    public static implicit operator TypeName(Type type) => new(type);
+    public static implicit operator TypeName(TypeReference typeRef) => new(typeRef);
+    public static implicit operator string(TypeName typeName) => typeName.Name;
+    public static bool operator ==(TypeName typeName, TypeName other) => typeName.Equals(other);
+    public static bool operator !=(TypeName typeName, TypeName other) => !(typeName == other);
+    public static bool operator ==(TypeName typeName, object other) => typeName.Equals(other);
+    public static bool operator !=(TypeName typeName, object other) => !(typeName == other);
+    public bool Equals(TypeName other) => Name.Equals(other.Name);
+    public override bool Equals(object? obj) => (obj is TypeName other && Equals(other)) || (obj is string str && str.Equals(Name));
+    public override int GetHashCode() => Name.GetHashCode();
+    public override string ToString() => Name;
+}
