397fbb5202f72339d58c9f48c96a2a77ed422a67
Gatongone <gatongone@gmail.com>
2023-11-20 23:20:57 +0800

    feature: add AssemblyHandler.

---
+++ Gneedle.Inject/Needle/CecilType.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Copyright ©2023 Gatongone
+ * Author: Gatongone
+ * Email: gatongone@gmail.com
+ * Created On: 2023/11/20-20:24:12
+ * Github: https://github.com/Gatongone
+ */
+namespace Gneedle.Inject;
+
+/// <summary>
+/// Cecil type info.
+/// </summary>
+/// <param name="definition">Type definition.</param>
+/// <param name="reference">Type reference.</param>
+internal readonly struct CecilType(TypeDefinition definition, TypeReference reference)
+{
+    /// <inheritdoc cref="Mono.Cecil.TypeDefinition"/>
+    public readonly TypeDefinition Definition;
+    
+    /// <inheritdoc cref="Mono.Cecil.TypeReference"/>
+    public readonly TypeReference Reference;
+}
